026dbd0ed1749c794b12176149c106be82d7187f
Joao Duarte <jsvduarte@gmail.com>
2017-03-16 04:45:56 -0700

    add multi_local source for multi pipelines

--- logstash-core/lib/logstash/bootstrap_check/default_config.rb
+++ logstash-core/lib/logstash/bootstrap_check/default_config.rb
@@ -4,12 +4,14 @@ require "logstash/errors"
 module LogStash module BootstrapCheck
   class DefaultConfig
     def self.check(settings)
+      # currently none of the checks applies if there are multiple pipelines
+      return if settings.get("pipelines").any?
+
       if settings.get("config.string").nil? && settings.get("path.config").nil?
         raise LogStash::BootstrapCheckError, I18n.t("logstash.runner.missing-configuration")
       end
 
       if settings.get("config.reload.automatic") && settings.get("path.config").nil?
-        # there's nothing to reload
         raise LogStash::BootstrapCheckError, I18n.t("logstash.runner.reload-without-config-path")
       end
     end
